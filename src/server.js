9acd24a7a1bcf50019400b61ab17bc456ac0004a
tsuryanto <tsuryanto16@gmail.com>
2023-12-26 20:51:13 +0700

    new: authentication service

--- src/server.js
+++ src/server.js
@@ -9,10 +9,15 @@ const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
 // users
-// users
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
+
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
 
 const ClientError = require('./exceptions/ClientError');
 
@@ -29,6 +34,7 @@ const init = async () => {
 
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   await server.register([
     {
@@ -43,6 +49,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
