5a32100ea0ec046f2648443c46ed6346fe728515
tsuryanto <tsuryanto16@gmail.com>
2023-12-26 21:27:08 +0700

    enc (authentication): add jwt validation for note api

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
 // notes
 const notes = require('./api/notes');
@@ -22,6 +23,10 @@ const AuthenticationsValidator = require('./validator/authentications');
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
+  const notesService = new NotesService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -32,9 +37,46 @@ const init = async () => {
     },
   });
 
-  const notesService = new NotesService();
-  const usersService = new UsersService();
-  const authenticationsService = new AuthenticationsService();
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+
+  // 'notesapp_jwt' = strategy name akan digunakan untuk menetapkan authentication pada routes.
+  // 'jwt' = nama skema yang akan digunakan pada pembuatan strategy
+  //         di sini kita memberikan nilai ‘jwt’ untuk menggunakan strategi jwt dari @hapi/jwt.
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+
+    // merupakan key atau kunci dari token JWT-nya (di mana merupakan access token key)
+    keys: process.env.ACCESS_TOKEN_KEY,
+
+    // merupakan objek yang menentukan seperti apa signature token JWT harus diverifikasi.
+    // nilai false di value dari object verify berarti tidak akan di verifikasi
+    verify: {
+      // nilai audience dari token
+      aud: false,
+      // nilai issuer dari token
+      iss: false,
+      // nilai subject dari token,
+      sub: false,
+      // nilai number yang menentukan umur kedaluwarsa dari token
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+
+    // merupakan fungsi yang membawa artifacts token.
+    // Fungsi ini dapat kita manfaatkan untuk menyimpan payload token
+    // yang berarti kredensial pengguna pada request.auth
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
 
   await server.register([
     {
